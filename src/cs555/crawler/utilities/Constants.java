d2b0e79d1dc51c75bbf0522ccfab7b11c37618cf
Luke Evans <levans002@gmail.com>
2012-11-23 22:51:37 -0700

    depth 2

--- src/cs555/crawler/utilities/Constants.java
+++ src/cs555/crawler/utilities/Constants.java
@@ -30,7 +30,7 @@ public class Constants {
 	public static final int Failure = 99;
 	public static final int Success = 100;
 	
-	public static final int depth = 1;
+	public static final int depth = 2;
 	
 	public static final int Page_Rank_Rounds = 1;
 	

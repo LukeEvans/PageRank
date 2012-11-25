8836e73869603b4c05dc581be28824f53994ae67
Luke Evans <levans002@gmail.com>
2012-11-25 13:02:41 -0700

    trying something

--- src/cs555/crawler/node/Worker.java
+++ src/cs555/crawler/node/Worker.java
@@ -327,10 +327,10 @@ public class Worker extends Node{
 
 		// Tell the node manager that we're done with this round
 		PageRankRoundComplete complete = new PageRankRoundComplete(serverPort);
-//		Link link = connect(nodeManager);
-//		link.sendData(complete.marshall());
-//		link.close();
-		managerLink.sendData(complete.marshall());
+		Link link = connect(nodeManager);
+		link.sendData(complete.marshall());
+		link.close();
+		//managerLink.sendData(complete.marshall());
 	}
 
 	//================================================================================

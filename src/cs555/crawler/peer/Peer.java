584ed528a2f4df3ec13adda76eb67232128f979a
Luke Evans <levans002@gmail.com>
2012-11-25 17:51:26 -0700

    testing

--- src/cs555/crawler/peer/Peer.java
+++ src/cs555/crawler/peer/Peer.java
@@ -33,7 +33,7 @@ public class Peer {
 	}
 	
 	public void sendData(byte[] bytes) {
-		if (link != null && !link.socket.isClosed()) {
+		if (link != null && !link.socket.isClosed() && !link.socket.isOutputShutdown()) {
 			link.sendData(bytes);
 		}
 	}
